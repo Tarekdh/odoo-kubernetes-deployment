59fd2ccc0f14d2143c240179bbb8c52ff8a885df
Tarek Khachnaoui <t.khachnaoui@oodrive.com>
2021-05-13 22:40:35 +0100

    odoo extra addons

---
+++ extra-addons/tar_module_api/__manifest__.py
@@ -0,0 +1,27 @@
+{ 
+    "name": "install api",
+    "summary": """Utility Features""",
+    "version": "12.0.3.0.4", 
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    "author": "Tarek Khachnaoui",
+    "contributors": [
+        "Tarek Khachnaoui <tarek.khachnawi@gmail.com>",
+    ],
+    "depends": [
+    ],
+    'data': [
+        'security/module_security.xml',
+    ],
+    "qweb": [
+    ],
+    "images": [
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "application": False,
+    "installable": True,
+    'auto_install': True,
+} 
